9521e7de0861e091b198cf6b055db918f31a9ff6
cchen008 <cchen008@citymail.cuny.edu>
2020-02-02 15:01:23 -0500

    Pushing default create-react-app

--- pedal/src/App.js
+++ pedal/src/App.js
@@ -1,24 +1,14 @@
 import React, {Component} from 'react';
-import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
  
 class App extends Component {
   render() {
     return (
       <div>
-        <Map google={this.props.google} zoom={14}>
-
-          <Marker onClick={this.onMarkerClick}
-            name={'Current location'} />
-
-          <InfoWindow onClose={this.onInfoWindowClose}>
-            
-          </InfoWindow>
-        </Map>
+        hello
       </div>
     );
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: ''
-})(App)
+export default App;
+
