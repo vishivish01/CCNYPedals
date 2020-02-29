73167ed975a51800259fd5f0ab498cb629e58773
Lisbeth Lazala <lisbethlazala@gmail.com>
2020-02-29 14:29:30 -0500

    reformatted map to use leaflet and be in index.html

--- pedal/src/App.js
+++ pedal/src/App.js
@@ -3,7 +3,8 @@ import ReactMapGL, { Marker } from 'react-map-gl';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
-const MAPBOX_TOKEN = 'pk.eyJ1IjoiY2NoZW4wMDgiLCJhIjoiY2s2NnYxN29wMWFjOTNvbzhmaDY3ZGxyYyJ9.VJqAlOFb4jz7AnbhadcuDQ';
+
+const MAPBOX_TOKEN = 'pk.eyJ1IjoibGxhemFsYSIsImEiOiJjazZwdmJ3OHExcW1yM2VueXB1NXcyNXU5In0.CMjAdPgsN5UGqO6yhwrtSQ';
 const someData = [
   {
     "bike_id":"7f5a4199-4f9d-490d-8ac2-2565ba501fe7",
@@ -23,70 +24,50 @@ const someData = [
     "lon":-77.01053,},
 ];
 
-setUserLocation = () => {
+// setUserLocation = () => {
 
-  navigator.geolocation.getCurrentPosition(position => {
-      let newViewport = {
-          height: "100vh",
-          width: "100vw",
-          latitude: position.coords.latitude,
-          longitude: position.coords.longitude,
-          zoom: 12
-      }
-      this.setState({
-          viewport: newViewport
-      })
-  })
-}
+//   navigator.geolocation.getCurrentPosition(position => {
+//       let newViewport = {
+//           height: "100vh",
+//           width: "100vw",
+//           latitude: position.coords.latitude,
+//           longitude: position.coords.longitude,
+//           zoom: 12
+//       }
+//       this.setState({
+//           viewport: newViewport
+//       })
+//   })
+// }
 
 class App extends Component {
-    state = {
-    viewport: {
-      latitude: 38.9072,
-      longitude: -77,
-      width: "100vw",
-      height: "100vh",
-      zoom: 12
-    }
-  };
+  // {someData.map(bird => (
+  //   <Marker
+  //     key={bird.bike_id}
+  //     latitude={bird.lat}
+  //     longitude={bird.lon}
+  //   >
+  //     <button>
+  //       <img src="https://cdn0.iconfinder.com/data/icons/bicycle-19/64/road-bike-bicycle-bike-riding-512.png" height={20} width={20} alt = "bike markers"></img>
+  //     </button>
+  //   </Marker>
+  // ))}
 
   render() {
-    return( 
-      <div>
-        <ReactMapGL
-          {...this.state.viewport}
-          mapboxApiAccessToken={MAPBOX_TOKEN}
-          mapStyle="mapbox://styles/cchen008/ck6fhdne30sjb1iqvxge9w504"
-          onViewportChange={viewport => 
-            this.setState( { viewport } ) }      
-        >
-          {someData.map(bird => (
-            <Marker 
-              key={bird.bike_id}
-              latitude={bird.lat}
-              longitude={bird.lon}
-            >
-              <button>
-                <img src="https://cdn0.iconfinder.com/data/icons/bicycle-19/64/road-bike-bicycle-bike-riding-512.png" height={20} width={20} alt = "bike markers"></img>
-              </button>
-            </Marker>
-          ))}
-          <Form style={{width:400}} >
-            <Form.Group controlId="formDestination">
-              <Form.Control type="text" placeholder="Enter your location" />
-            </Form.Group>
-            <Form.Group controlId="formDestination">
-              <Form.Control type="text" placeholder="Enter your destination" />
-              <Button>
-                Search
-              </Button>
-            </Form.Group>
-          </Form>
-        </ReactMapGL>
-      </div>
+    return(
+        <Form style={{width:400}} >
+          <Form.Group controlId="formDestination">
+            <Form.Control type="text" placeholder="Enter your location" />
+          </Form.Group>
+          <Form.Group controlId="formDestination">
+            <Form.Control type="text" placeholder="Enter your destination" />
+            <Button id="search">
+              Search
+            </Button>
+          </Form.Group>
+        </Form>
     );
-  }
-
+ }
 }
 
 export default App;
