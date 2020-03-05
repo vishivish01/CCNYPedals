1f19f821fe2f4d6bcc08a5ac806a38ffe51183b0
Lisbeth Lazala <lisbethlazala@gmail.com>
2020-03-05 11:38:04 -0500

    added mising file to pr

--- pedal/src/routing.js
+++ pedal/src/routing.js
@@ -1,32 +1,3 @@
-// import {MapLayer, withLeaflet} from 'react-leaflet';
-// import L from 'leaflet';
-// import 'leaflet-routing-machine';
-//
-// class RoutingMachine extends MapLayer {
-//     componentDidMount() {
-//         super.componentDidMount();
-//         this.leafletElement.addTo(this.props.map);
-//     }
-//
-//     render() {
-//         return null;
-//     }
-//
-//     createLeafletElement (props) {
-//         const {from, to} = this.props;
-//         console.log(this.props)
-//         var leafletElement = L.Routing.control({
-//             waypoints: [
-//                 L.latLng(from[0], from[1]),
-//                 L.latLng(to[0], to[1]),
-//             ],
-//         });
-//         return leafletElement;
-//     }
-// }
-//
-// export default withLeaflet(RoutingMachine);
-
 import { MapLayer } from "react-leaflet";
 import L from "leaflet";
 import "leaflet-routing-machine";
@@ -37,13 +8,11 @@ class Routing extends MapLayer {
   createLeafletElement() {
     const {from, to} = this.props;
     const { map } = this.props;
-    console.log(this.props)
     let leafletElement = L.Routing.control({
       waypoints: [
         L.latLng(from[0], from[1]),
         L.latLng(to[0], to[1]) //we can add another waypoint here
       ],
-      // router: new L.Routing.Google(),
       lineOptions: {
         styles: [
           {
