ef7b6a3a3096b68eb4726458631f32db1c25e163
cchen008 <cchen008@citymail.cuny.edu>
2020-05-11 14:49:04 -0400

    Adding train icon #16

--- pedal/src/Icon.js
+++ pedal/src/Icon.js
@@ -3,21 +3,31 @@ import L from 'leaflet';
 const birdIcon = new L.Icon({
     iconUrl: require('./markers/bird.png'),
     iconAnchor: null,
-    popupAnchor: null,
+    popupAnchor: [0, -20],
     shadowUrl: null,
     shadowSize: null,
     shadowAnchor: null,
-    iconSize: new L.Point(30, 30),
+    iconSize: new L.Point(40, 40),
 });
 
 const lyftIcon = new L.Icon({
     iconUrl: require('./markers/lyft.png'),
     iconAnchor: null,
-    popupAnchor: null,
+    popupAnchor: [0, -20],
     shadowUrl: null,
     shadowSize: null,
     shadowAnchor: null,
-    iconSize: new L.Point(30, 30),
+    iconSize: new L.Point(40, 40),
 });
 
-export { birdIcon, lyftIcon };
+const trainIcon = new L.Icon({
+    iconUrl: "https://i.pinimg.com/236x/cf/58/4a/cf584aff36fb87c055a8a026bfcb859e--moodboard-trains.jpg",
+    iconAnchor: null,
+    popupAnchor: [0, -20],
+    shadowUrl: null,
+    shadowSize: null,
+    shadowAnchor: null,
+    iconSize: new L.Point(40, 40),
+});
+
+export { birdIcon, lyftIcon, trainIcon }
